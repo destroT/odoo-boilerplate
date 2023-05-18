9a88d0d4b44990ff8eac8e2cab500e9479fd0347
DestroT <destro.tommaso@gmail.com>
2023-05-18 10:27:46 +0000

    [FIX] removed sed on requirements

--- setup.sh
+++ setup.sh
@@ -40,9 +40,6 @@ cd $DIRPATH
 git clone https://github.com/odoo/odoo.git -b 14.0 --depth=1 --single-branch
 sh $DIRPATH/gitpull.sh
 
-# Prepare reuirements for odoo 3.8
-sed -i -e "/< '3.7'/d" -e "/<\= '3.7'/d" -e "/\=\= '3.7'/d" -e "/< '3.8'/d"  -e "/\=\= 'win32'$/d" $DIRPATH/odoo/requirements.txt
-
 # Prepare virtualenv
 pip install --upgrade pip
 python3 -m venv venv
